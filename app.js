46d6891908c63acd02dc22739433b7440fe3d15e
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2024-06-11 17:35:08 +0900

    EJS - Functionality with Express

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function(req, res) {
-    res.send("<html><body>Portal de noticias</body></html>");
-});
+app.set('view engine', 'ejs');
 
 app.get('/tecnologia', function(req, res) {
-    res.send("<html><body>Noticias de tecnologia</body></html>");
+    res.render("secao/tecnologia");
+});
+
+app.get('/', function(req, res) {
+    res.send("<html><body>Portal de noticias</body></html>");
 });
 
 app.listen(3000, function(){
